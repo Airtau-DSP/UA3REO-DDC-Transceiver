b399005b779f61b116f2637ce3adb14f53665baf
XGudron <XGudron@gmail.com>
2019-04-18 23:05:27 +0300

    AGC comments

--- STM32/Src/agc.c
+++ STM32/Src/agc.c
@@ -12,6 +12,7 @@ float32_t RX_AGC_STEPSIZE_DOWN=1.0f;
 	
 void InitAGC(void)
 {
+	//выше скорость в настройках - выше скорость отработки AGC
 	RX_AGC_STEPSIZE_UP=5000.0f/(float32_t)TRX.Agc_speed;
 	RX_AGC_STEPSIZE_DOWN=50.0f/(float32_t)TRX.Agc_speed;
 }
@@ -21,11 +22,13 @@ void DoAGC(float32_t *agcBuffer, int16_t blockSize)
 	float32_t AGC_RX_MAX_amplitude=0;
 	uint32_t AGC_RX_MAX_amplitude_index=0;
 	float32_t step=0;
-	
+
+	//ищем максимум в амплитуде
 	arm_max_f32(agcBuffer,blockSize,&AGC_RX_MAX_amplitude,&AGC_RX_MAX_amplitude_index);
 	if(AGC_RX_MAX_amplitude==0.0f) AGC_RX_MAX_amplitude=0.001f;
+	//расчитываем целевое значение усиления
 	float32_t AGC_need_gain_target = AGC_OPTIMAL_THRESHOLD/AGC_RX_MAX_amplitude;
-	
+	//двигаем усиление на шаг
 	if(AGC_need_gain_target>AGC_need_gain)
 	{
 		step=(AGC_need_gain_target-AGC_need_gain)/RX_AGC_STEPSIZE_UP;
@@ -38,11 +41,11 @@ void DoAGC(float32_t *agcBuffer, int16_t blockSize)
 		AGC_need_gain -= step;
 	}
 	if(AGC_need_gain<0.0f) AGC_need_gain=0.0f;
-	
+	//перегрузка (клиппинг), резко снижаем усиление
 	if((AGC_need_gain*AGC_RX_MAX_amplitude)>AGC_CLIP_THRESHOLD)
 	{
 		AGC_need_gain = AGC_need_gain_target;
 	}
-	
+	//применяем усиление
 	arm_scale_f32(agcBuffer, AGC_need_gain, agcBuffer, blockSize);
 }
