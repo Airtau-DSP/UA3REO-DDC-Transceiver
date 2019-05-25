9f299b2a661ef54ec2aa66043c846f87a3a12db9
XGudron <XGudron@gmail.com>
2019-05-25 21:44:10 +0300

    Timer fix

--- STM32/Src/main.c
+++ STM32/Src/main.c
@@ -595,9 +595,9 @@ static void MX_TIM6_Init(void)
 
   /* USER CODE END TIM6_Init 1 */
   htim6.Instance = TIM6;
-  htim6.Init.Prescaler = 42000;
+  htim6.Init.Prescaler = 41999;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 100;
+  htim6.Init.Period = 99;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
